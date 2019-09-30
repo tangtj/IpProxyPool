116572a9bd1ca1ba5ac9c86112022e75a8dd3acd
tangjie <JDNet_tangjie@git.cloud.tencent.com>
2019-09-30 14:32:40 +0800

    新增基础异常信息类

---
+++ src/main/java/cn/tangtj/proxypool/exception/BaseException.java
@@ -0,0 +1,8 @@
+package cn.tangtj.proxypool.exception;
+
+/**
+ * @author tang
+ * @date 2019/9/30
+ */
+public abstract class BaseException extends RuntimeException {
+}
--- src/main/java/cn/tangtj/proxypool/exception/RequestException.java
+++ src/main/java/cn/tangtj/proxypool/exception/RequestException.java
@@ -4,5 +4,5 @@ package cn.tangtj.proxypool.exception;
  * @author tang
  * @date 2019/9/28
  */
-public class RequestException extends RuntimeException {
+public class RequestException extends BaseException {
 }
